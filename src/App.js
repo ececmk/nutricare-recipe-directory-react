8cd2d8baeef3a737b823b01e1c57da75a59fc698
Ece Çakmak <86318154+ececmk@users.noreply.github.com>
2023-02-27 14:48:39 +0100

    routers done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 import './App.css';
+import Home from './Pages/Home/Home'
+import Create from './Pages/Create/Create'
+import Recipe from './Pages/Recipe/Recipe'
+import Search from './Pages/Search/Search'
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/recipes/:id" element={<Recipe />} />
+    </Routes>
     </div>
   );
 }

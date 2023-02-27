b009c362ff1338bc9f6bf65df5d4b1661b9aa0ac
Ece Çakmak <86318154+ececmk@users.noreply.github.com>
2023-02-27 16:58:37 +0100

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import { Routes, Route } from "react-router-dom";
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// page components
 import Home from './Pages/Home/Home'
 import Create from './Pages/Create/Create'
-import Recipe from './Pages/Recipe/Recipe'
 import Search from './Pages/Search/Search'
+import Recipe from './Pages/Recipe/Recipe'
+
+// styles
+import './App.css'
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create" element={<Create />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/recipes/:id" element={<Recipe />} />
-    </Routes>
+    <BrowserRouter>
+    <Navbar />
+       <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/recipe/:id" element = {<Recipe />} />
+        </Routes>
+        </BrowserRouter>
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+
+
+import { Link } from 'react-router-dom'
+
+// styles
+import './Navbar.css'
+
+const Navbar= () => {
+  return (
+    <div className="navbar">
+      <nav>
+        <Link to="/" className="brand">
+          <h1>NutriCare</h1>
+        </Link>
+        <Link to="/create">Create Recipe</Link>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar;
